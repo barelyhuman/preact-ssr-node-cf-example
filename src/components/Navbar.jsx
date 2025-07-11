3c8b02c103e4964a134889a313561aa71c4dc5f6
reaper <ahoy@barelyhuman.dev>
2025-07-11 12:21:08 +0530

    chore: remove Link , it's unneeded

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,3 @@
-import { Link } from "./Link";
-
 export const Navbar = () => {
   return (
     <header>
@@ -14,21 +12,21 @@ export const Navbar = () => {
           >
             <ul class="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
               <li>
-                <Link
-                  to="/"
+                <a
+                  href="/"
                   class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0"
                   aria-current="page"
                 >
                   Home
-                </Link>
+                </a>
               </li>
               <li>
-                <Link
-                  to="/counter"
+                <a
+                  href="/counter"
                   class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0"
                 >
                   Counter
-                </Link>
+                </a>
               </li>
             </ul>
           </div>
